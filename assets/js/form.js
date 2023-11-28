232871f8d9fd5b0d6803c830b63cbb4492d1c5ae
Sydney Spaulding <cozysydmedia@gmail.com>
2023-11-27 22:15:58 -0800

    debugged validation

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,7 +1,12 @@
 function validateForm() {
-    let x = document.forms["myForm"]["fname"].value;
+    let x = document.forms["newsletterForm"]["name"].value;
     if (x == "") {
       alert("Name must be filled out");
+      return false;
+    }
+    let y = document.forms["newsletterForm"]["email"].value;
+    if (y == "") {
+      alert("Email must be filled out");
       return false;
     }
   }
